ea7d24f6c94047fd3a0f58021c65c8efd649b8b2
José Luis Herrera <jose.herrera@nter.es>
2024-07-05 13:42:08 +0200

    code and styles

---
+++ script.js
@@ -0,0 +1,87 @@
+let isOn = false;
+let memory = 0;
+const MAX_CHARACTERS = 11;
+
+const turnOn = () => {
+  isOn = true;
+  document.getElementById("screen").disabled = false;
+  clearScreen();
+};
+
+const turnOff = () => {
+  isOn = false;
+  document.getElementById("screen").disabled = true;
+  clearScreen();
+};
+
+const addToScreen = (character) => {
+  if (isOn) {
+    let screen = document.getElementById("screen");
+    let currentValue = screen.value;
+
+    if (character === "." && currentValue.includes(".")) {
+      return;
+    }
+
+    const operators = ["+", "-", "*", "/", "x", "÷"];
+    if (
+      operators.includes(character) &&
+      operators.includes(currentValue.slice(-1))
+    ) {
+      return;
+    }
+
+    if (currentValue.length < MAX_CHARACTERS) {
+      screen.value += character;
+    }
+  }
+};
+
+const clearScreen = () => {
+  if (isOn) {
+    document.getElementById("screen").value = "";
+  }
+};
+
+const memoryPlus = () => {
+  if (isOn) {
+    memory = parseFloat(document.getElementById("screen").value) || 0;
+    clearScreen();
+  }
+};
+
+const memoryClear = () => {
+  if (isOn) {
+    memory = 0;
+  }
+};
+
+const memoryRecall = () => {
+  if (isOn) {
+    document.getElementById("screen").value = memory;
+  }
+};
+
+const calculate = () => {
+  if (isOn) {
+    let screen = document.getElementById("screen");
+    let operation = screen.value;
+    let result;
+
+    if (operation === "") {
+      result = 0;
+    } else {
+      try {
+        operation = operation
+          .replace("%", "/100")
+          .replace("x", "*")
+          .replace("÷", "/");
+        result = eval(operation);
+      } catch (error) {
+        result = "Error";
+      }
+    }
+
+    screen.value = result;
+  }
+};
